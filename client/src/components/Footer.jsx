786d1a99612dc8f62367bde85dcd6d832f3b3f50
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-10-12 10:16:34 +0530

    Footer before pulling another branch

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,37 +4,32 @@ function Footer() {
   return (
     <div className='footer f1 bg-white p-12 flex flex-col gap-8'>
        
-    <div className='footer flex flex-col md:flex-row justify-center gap-12'>
+    <div className='footer flex flex-col md:flex-row justify-center gap-8 md:gap-12'>
     {/** Border point */}
     <div className='1st'>
     <p className='font-bold font-outfit text-4xl'>CampusX</p>
-    <p className='font-inter mt-4'>Build. Share. Collaborate <br /> For Developers, By Developers</p>
-   <button className='bg-[#6a7cff] py-3 px-6 mt-2 rounded-xl text-white font-inter font-medium'>Email Us</button>
+    <p className='font-inter mt-3'>Build. Share. Collaborate <br /> For Developers, By Developers</p>
+   <button className='bg-[#6a7cff] py-3 px-6 mt-2 rounded-lg text-white font-inter font-medium'>Email Us</button>
     </div>
     {/** Border point */}
     <div className='2nd flex flex-col md:flex-row gap-8'>
     <div className='col-1'>
-    <p className='font-bold font-outfit text-2xl'>About</p>
-    <p className='font-inter mt-4 cursor-pointer'>/techstack</p>
-    <p className='font-inter mt-4 cursor-pointer'>/techstack</p>
-    <p className='font-inter mt-4'>/techstack</p>
+    <p className='font-bold font-inter text-2xl'>Product</p>
+    <p className='font-inter mt-3 cursor-pointer hover:underline'>CampusX</p>
+    <p className='font-inter mt-3 cursor-pointer hover:underline'>CampusAI</p>
+    <p className='font-inter mt-3 cursor-pointer hover:underline'>About Us</p>
     </div>
     <div className='col-2'>
-    <p className='font-bold font-outfit text-2xl'>Products</p>
-    <p className='font-inter mt-4 cursor-pointer'>/techstack</p>
-    <p className='font-inter mt-4 cursor-pointer'>/techstack</p>
-    <p className='font-inter mt-4'>/techstack</p>
+    <p className='font-bold font-inter text-2xl'>Support</p>
+    <p className='font-inter mt-3 cursor-pointer hover:underline'>Help Center</p>
+    <p className='font-inter mt-3 cursor-pointer hover:underline'>Open Source</p>
+    <p className='font-inter mt-3 cursor-pointer hover:underline'>Sponsor</p>
     </div>
-    <div className='col-3'>
-    <p className='font-bold font-outfit text-2xl'>Contact Us</p>
-    <p className='font-inter mt-4 cursor-pointer'>/techstack</p>
-    <p className='font-inter mt-4 cursor-pointer'>/techstack</p>
-    <p className='font-inter mt-4'>/techstack</p>
-    </div>
+    
     </div>
     {/** Border point */}
     <div className='3rd flex flex-col'>
-    <p className='font-bold font-outfit text-4xl'>Build By</p>
+    <p className='font-bold font-inter text-2xl'>Built By</p>
     <button className='bg-[#6a7cff] py-3 px-6 mt-4 rounded-xl text-white font-inter font-medium'>Adnan Khan</button>
     <button className='bg-[#6a7cff] py-3 px-6 mt-2 rounded-xl text-white font-inter font-medium'>Garv Thakre</button>
     </div>
